d9af8fce8782261e74b69468d7a8cd5b24526dd7
Gluckasz <lukas.gluck.04@gmail.com>
2024-12-10 19:36:53 +0100

    Added the database model

---
+++ apn-promise-recruiting-task/Model.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace apn_promise_recruiting_task
+{
+
+
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<OrderITem> OrderITems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        public string DbPath { get; }
+
+        public ApplicationDbContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "orders.db");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source={DbPath}");
+    }
+
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public required string Name { get; set; }
+        public required int Price { get; set; }
+        public required string Currency { get; set; }
+    }
+
+    public class OrderITem
+    {
+        public int OrderITemId { get; set; }
+        public required Product Product { get; set; }
+    }
+
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public required List<Order> Orders { get; set; }
+    }
+}
--- apn-promise-recruiting-task/Program.cs
+++ apn-promise-recruiting-task/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using apn_promise_recruiting_task;
+using Microsoft.EntityFrameworkCore;
+
+using (var context = new ApplicationDbContext())
+{
+    context.Database.Migrate();
+}
